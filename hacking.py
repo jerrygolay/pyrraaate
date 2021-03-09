1434c51332b45c3894293a83c7afbaaf6d043322
jerry golay <jeremy.khalfa2017@campus-eni.fr>
2021-03-09 09:02:33 +0100

    Script V2

---
+++ hacking.py
@@ -0,0 +1,16 @@
+#/usr/bin/python3.9
+# coding : utf-8
+import time
+
+value = "."
+c = 0
+print("Hacking in progress")
+
+while c != 5:
+	print(value)
+	value += "."
+	c += 1
+	time.sleep(1)
+
+print("Hacking finish")
+

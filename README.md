9abaf1196d8c9e5f75d2e6498e56f7c263bde408
root <jeremy.khalfa2017@campus-eni.fr>
2021-03-09 08:44:22 +0100

    First hacking script

---
+++ README.md
@@ -0,0 +1,3 @@
+# My python hacking script
+
+
---
+++ script.py
@@ -0,0 +1,22 @@
+#/usr/bin/python3
+
+import time
+
+print("Hacking in progress")
+time.sleep(1)
+print(".")
+time.sleep(1)
+print("..")
+time.sleep(1)
+print("...")
+time.sleep(1)
+print("....")
+time.sleep(1)
+print(".....")
+time.sleep(1)
+print("......")
+time.sleep(1)
+print(".......")
+time.sleep(1)
+print("Hacking finish")
+
